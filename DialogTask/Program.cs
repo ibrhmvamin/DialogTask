2c20c7b1ea2d042744575407d29ff1044698ad79
ibrhmvamin <aminibrahimov52@gmail.com>
2023-12-03 12:35:17 +0400

    Add project files.

---
+++ DialogTask/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace DialogTask
+{
+    public class Program
+    {
+        public interface IButton
+        {
+            void Render();
+            void OnClick();
+
+        }
+
+        public class WindowsButton : IButton
+        {
+            public void OnClick()
+            {
+                Console.WriteLine("Clicked in Windows");
+            }
+
+            public void Render()
+            {
+                Console.WriteLine("Rendered in Windows");
+            }
+        }
+
+        public class HTMLButton : IButton
+        {
+            public void OnClick()
+            {
+                Console.WriteLine("Clicked in HTML");
+            }
+
+            public void Render()
+            {
+                Console.WriteLine("Rendered in HTML");
+            }
+        }
+
+        public abstract class Dialog
+        {
+            public void Render()
+            {
+                IButton? okbutton = CreateButton();
+                okbutton.Render();
+                okbutton.OnClick();
+            }
+            public virtual IButton? CreateButton() => null;
+        }
+        public class WindowsDialog : Dialog
+        {
+            public override IButton? CreateButton()
+            {
+                return new WindowsButton();
+            }
+        }
+
+
+        public class WebDialog : Dialog
+        {
+            public override IButton? CreateButton()
+            {
+                return new HTMLButton();
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
